b7b22826ba36e363b1f6e98f3ce6f794aff2013b
KomiljonovDev <uzokdeveloper@gmail.com>
2022-10-26 22:02:04 -0700

    almost finished answers methods

---
+++ API/v1/helpers/Models/Answer.php
@@ -0,0 +1,261 @@
+<?php
+
+	require_once './config/config.php';
+	require_once './helpers/functions.php';
+
+	class Answer extends db_mysqli
+	{
+		use Helper;
+
+		public function __construct()
+		{
+			$this->dbConnect();
+			$this->extract($_REQUEST);
+		}
+
+		public function getAnswersById()
+		{
+			if (!isset($this->question_id)) {
+				$this->data['code'] = 400;	
+				$this->data['message'] = 'question id (question_id) is required';
+				return $this->data;
+			}
+			$post = $this->selectWhere('answers',[
+				[
+					'question_id'=>$this->question_id,
+					'cn'=>'='
+				],
+			]);
+			if ($post->num_rows) {
+				$this->data['ok'] = true;	
+				$this->data['code'] = 201;	
+				$this->data['message'] = 'Answers of question gived successfully';
+				foreach ($post as $key => $value) $this->data['result'][$key] = $value;
+				return $this->data;
+			}else{
+				$this->data['code'] = 401;	
+				$this->data['message'] = 'question_id is invalid';
+				return $this->data;
+			}
+		}
+
+		public function getAnswersByBetween()
+		{
+			if (!isset($this->question_id)) {
+				$this->data['code'] = 400;	
+				$this->data['message'] = 'question id (question_id) is required';
+				return $this->data;
+			}
+			$from = isset($this->from) ? $this->from : 1; 
+			$to = isset($this->to) ? $this->to : 10; 
+			$this->data['ok'] = true;	
+			$this->data['code'] = 201;	
+			$this->data['message'] = 'Answers gived successfully';
+			$question_answers = $this->selectWhere('answers',[
+				[
+					'id'=>$from,
+					'cn'=>'>='
+				],
+				[
+					'id'=>$to,
+					'cn'=>'<='
+				],
+				[
+					'question_id'=>$this->question_id,
+					'cn'=>'<='
+				],
+			]);
+			foreach ($question_answers as $key => $value) $this->data['result'][$key] = $value;
+			return $this->data;
+		}
+
+		public function insertAnswer()
+		{
+
+			if (!isset($this->token)) {
+				$this->data['code'] = 400;	
+				$this->data['message'] = 'token is required';
+				return $this->data;
+			}
+			$user = $this->selectWhere('users',[
+				[
+					'token'=>$this->token,
+					'cn'=>'='
+				]
+			]);
+			if (!$user->num_rows) {
+				$this->data['code'] = 401;	
+				$this->data['message'] = 'token is invalid';
+				return $this->data;
+			}
+			if (!isset($this->question_id)) {
+				$this->data['code'] = 400;	
+				$this->data['message'] = 'question id (question_id) is required';
+				return $this->data;
+			}
+			$post = $this->selectWhere('questions',[
+				[
+					'id'=>$this->question_id,
+					'cn'=>'='
+				]
+			]);
+			if (!$post->num_rows) {
+				$this->data['code'] = 401;	
+				$this->data['message'] = 'question_id is invalid';
+				return $this->data;
+			}
+			if (!isset($this->title) || !isset($this->body)) {
+				$this->data['code'] = 400;	
+				$this->data['message'] = 'title and body are required';
+				return $this->data;
+			}
+			$user = mysqli_fetch_assoc($user);
+			$this->insertInto('answers',[
+				'question_id'=>$this->question_id,
+				'user_id'=>$user['id'],
+				'title'=>$this->title,
+				'body'=>$this->body,
+				'is_true'=>false,
+				'created_at'=>strtotime('now'),
+				'updated_at'=>''
+			]);
+			$this->data['ok'] = true;	
+			$this->data['code'] = 201;	
+			$this->data['message'] = 'Answer successfully inserted';
+			$question_answers = $this->selectWhere('answers',[
+				[
+					'question_id'=>$this->question_id,
+					'cn'=>'='
+				]
+			]);
+			foreach ($question_answers as $key => $value) $this->data['result'][$key] = $value;
+			return $this->data;
+		}
+
+		public function updateAnswer()
+		{
+			if (!isset($this->token)) {
+				$this->data['code'] = 400;	
+				$this->data['message'] = 'token is required';
+				return $this->data;
+			}
+			$user = $this->selectWhere('users',[
+				[
+					'token'=>$this->token,
+					'cn'=>'='
+				]
+			]);
+			if (!$user->num_rows) {
+				$this->data['code'] = 401;	
+				$this->data['message'] = 'token is invalid';
+				return $this->data;
+			}
+			if (!isset($this->answer_id)) {
+				$this->data['code'] = 400;	
+				$this->data['message'] = 'answer id (answer_id) is required';
+				return $this->data;
+			}
+			$answer = $this->selectWhere('answers',[
+				[
+					'id'=>$this->answer_id,
+					'cn'=>'='
+				]
+			]);
+			if (!$answer->num_rows) {
+				$this->data['code'] = 401;	
+				$this->data['message'] = 'answer_id is invalid';
+				return $this->data;
+			}
+			$user = mysqli_fetch_assoc($user);
+			$answer = mysqli_fetch_assoc($answer);
+			if ($user['id'] != $answer['user_id']) {
+				$this->data['code'] = 403;	
+				$this->data['message'] = 'Answer does not belong to this user';
+				return $this->data;
+			}
+			$title = isset($this->title) ? $this->title : $answer['title'];
+			$body = isset($this->body) ? $this->body : $answer['body'];
+			$this->update('answers',[
+				'title'=>$title,
+				'body'=>$body,
+				'updated_at'=>strtotime('now'),
+			],[
+				'id'=>$this->answer_id,
+				'cn'=>'='
+			]);
+			$this->data['ok'] = true;	
+			$this->data['code'] = 201;	
+			$this->data['message'] = 'Answer successfully updated';
+			$question_answers = $this->selectWhere('answers',[
+				[
+					'question_id'=>$answer['question_id'],
+					'cn'=>'='
+				]
+			]);
+			foreach ($question_answers as $key => $value) $this->data['result'][$key] = $value;
+			return $this->data;
+		}
+
+		public function deleteAnswer()
+		{
+			if (!isset($this->token)) {
+				$this->data['code'] = 400;	
+				$this->data['message'] = 'token is required';
+				return $this->data;
+			}
+			$user = $this->selectWhere('users',[
+				[
+					'token'=>$this->token,
+					'cn'=>'='
+				]
+			]);
+			if (!$user->num_rows) {
+				$this->data['code'] = 401;	
+				$this->data['message'] = 'token is invalid';
+				return $this->data;
+			}
+			if (!isset($this->answer_id)) {
+				$this->data['code'] = 400;	
+				$this->data['message'] = 'answer id (answer_id) is required';
+				return $this->data;
+			}
+			$answer = $this->selectWhere('answers',[
+				[
+					'id'=>$this->answer_id,
+					'cn'=>'='
+				]
+			]);
+			if (!$answer->num_rows) {
+				$this->data['code'] = 401;	
+				$this->data['message'] = 'answer_id is invalid';
+				return $this->data;
+			}
+			$user = mysqli_fetch_assoc($user);
+			$answer = mysqli_fetch_assoc($answer);
+			if ($user['id'] != $answer['user_id']) {
+				$this->data['code'] = 403;	
+				$this->data['message'] = 'Answer does not belong to this user';
+				return $this->data;
+			}
+			$this->delete('answers',[
+				[
+					'id'=>$this->answer_id,
+					'cn'=>'='
+				],
+			]);
+			$this->data['ok'] = true;	
+			$this->data['code'] = 200;	
+			$this->data['message'] = 'Answer successfully deleted';
+			$question_answers = $this->selectWhere('answers',[
+				[
+					'question_id'=>$answer['question_id'],
+					'cn'=>'='
+				]
+			]);
+			foreach ($question_answers as $key => $value) $this->data['result'][$key] = $value;
+			return $this->data;
+		}
+	}
+
+
+?>
--- API/v1/index.php
+++ API/v1/index.php
@@ -10,15 +10,17 @@
 		require_once 'helpers/Models/Post.php';
 		require_once 'helpers/Models/Comment.php';
 		require_once 'helpers/Models/Question.php';
+		require_once 'helpers/Models/Answer.php';
 		$db = new db_mysqli;
 		$db->dbConnect();
 
 		$post = new Post;
 		$comment = new Comment;
 		$question = new Question;
+		$answer = new Answer;
 
 		$action = strtolower(trim(getenv('ORIG_PATH_INFO') ? : getenv('PATH_INFO'), '/'));
-		if ($action == 'insertpost') {
+		if ($action == 'insertpost') { // POST
 			$data = $post->insertPost();
 		}else if($action == 'getposts'){
 			$data = $post->getAllPost();
@@ -34,7 +36,7 @@
 			$data = $post->deletePost();
 		}else if($action == 'viewpost'){
 			$data = $post->viewPost();
-		}else if($action == 'insertcomment'){
+		}else if($action == 'insertcomment'){ // COMMENT
 			$data = $comment->insertComment();
 		}else if($action == 'getcommentsbyid'){
 			$data = $comment->getCommentsById();
@@ -44,7 +46,7 @@
 			$data = $comment->updateComment();
 		}else if($action == 'deletecomment'){
 			$data = $comment->deleteComment();
-		}else if($action == 'insertquestion'){
+		}else if($action == 'insertquestion'){ // QUESTION
 			$data = $question->insertQuestion();
 		}else if($action == 'updatequestion'){
 			$data = $question->updateQuestion();
@@ -52,6 +54,14 @@
 			$data = $question->getQuestionsByBetween();
 		}else if($action == 'deletequestion'){
 			$data = $question->deleteQuestion();
+		}else if($action == 'insertanswer'){ // ANSWER
+			$data = $answer->insertAnswer();
+		}else if($action == 'updateanswer'){
+			$data = $answer->updateAnswer();
+		}else if($action == 'getanswersbyid'){
+			$data = $answer->getAnswersById();
+		}else if($action == 'getanswersbybetween'){
+			$data = $answer->getAnswersByBetween();
 		}else if(false){
 
 		}else{
